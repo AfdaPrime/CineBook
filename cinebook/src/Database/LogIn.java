bfeac3ef7ceb015b891007cd283c04c7822525d3
Muhammad Idzhans Khairi <muhammadidzhanskhairi@gmail.com>
2021-12-20 00:23:18 +0800

    Login To Database

--- cinebook/src/Database/LogIn.java
+++ cinebook/src/Database/LogIn.java
@@ -20,7 +20,7 @@ public class LogIn {
             int count = 0, status = 0;
             boolean status_data = true;
             String username, password;
-            String SQL = "SELECT * FROM CUSTOMER";
+            String SQL = "SELECT * FROM TESTING_NOW";
             String host = "jdbc:derby://localhost:1527/CinemaBooking_Database";
             String uName = "WIX1002_OCC5_02";
             String uPass = "siapaapplesebenarnya";
@@ -52,24 +52,41 @@ public class LogIn {
                             status_data = true;
                         }
                         
-                        String username_data = rs.getString("Username_Customer");
-                        String pwd = rs.getString("Password_Customer");
-                    
+                        String username_data = rs.getString("Username");
+                        String pwd = rs.getString("Password");
+                        
+                        //Both info is equals to the input
                         if(username.equals(username_data) && password.equals(pwd)) 
                         {
                             status = 1;
                             break finding;
                         }
+                        //Either one of the info is equals to the input
+                        else if((!username.equals(username_data) && password.equals(pwd)) || (username.equals(username_data) && !password.equals(pwd)))
+                        {
+                            System.out.println("\n--------------------------------------------------------------------------");
+                            System.out.println("The Username or Password you entered does not match!!!\n");
+                            break;
+                        }
+                        //Both of the info is not equals
+                        else if((!username.equals(username_data) && !password.equals(pwd)) && rs.next() == false) 
+                        {
+                            System.out.println("\n--------------------------------------------------------------------------");
+                            System.out.println("The Username and Password you entered does not exist!!!\n");
+                            break;
+                        }
+                        //The statement below needed because everytime it passes 2nd if else, it skipped one line because of rs.next()
+                        rs.previous();
+                        
                     }
                     
-                    status_data = false;
-                    System.out.println("\n------------------------------------------------------");
-                    System.out.println("The username or password you entered does not exist!!!\n");
                     System.out.println("Please re-enter your Username and Password : ");
                     System.out.print("Username : ");
                     username = input.nextLine();
                     System.out.print("Password : ");
                     password = input.nextLine();
+                    
+                    status_data = false;
                     rs.first();
                 }
             }
