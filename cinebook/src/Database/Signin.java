e66c1c24e593840499aacbca93210e3c2281e9a0
Muhammad Idzhans Khairi <muhammadidzhanskhairi@gmail.com>
2021-12-21 01:33:32 +0800

    Sign In

---
+++ cinebook/src/Database/Signin.java
@@ -0,0 +1,62 @@
+package Database;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class Signin {
+
+    //In This Database, there are 4 example users : 
+    //Username 1 : Muhammad  |  Password 1 : muhammad01
+    //Username 2 : Idzhans   |  Password 2 : idzhans01
+    //Username 3 : Khairi    |  Password 3 : khairi01
+    //Username 4 : Afda      |  Password 4 : afda02
+    //Username 5 : Afiq      |  Password 5 : afiq02  (newly entered)
+    public static void main(String[] args) {
+        
+        try {
+            
+            String username, password;
+            int curRow = 0;
+            Scanner input = new Scanner(System.in);
+            
+            String SQL = "SELECT * FROM TESTING_NOW";
+            String host = "jdbc:derby://localhost:1527/CinemaBooking_Database";
+            String uName = "WIX1002_OCC5_02";
+            String uPass = "siapaapplesebenarnya";
+        
+            //Set up a connection to the database
+            Connection con = DriverManager.getConnection(host, uName, uPass);
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery(SQL);
+            
+            System.out.println("Please enter the following to sign in : ");
+            System.out.print("Username : ");
+            username = input.nextLine();
+            System.out.print("Password : ");
+            password = input.nextLine();
+            
+            rs.moveToInsertRow();
+            
+            rs.updateString("Username", username);
+            rs.updateString("Password", password);
+            //Inserts a new row
+            rs.insertRow();
+            //Close a statement to update database
+            rs.close();
+            
+            System.out.println("\nYou have signed in to GSC! Welcome!!!\n");
+            
+            
+        }
+        catch(SQLException err) {
+            
+            System.out.println(err.getMessage());
+            
+        }
+        
+    }
+    
+}
